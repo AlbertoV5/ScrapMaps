ea112334192f0428f47afa34ed2764464c0388a4
AlbertoV5 <58243333+AlbertoV5@users.noreply.github.com>
2020-08-06 21:36:52 -0500

    new

--- osuRanked.py
+++ osuRanked.py
@@ -8,69 +8,59 @@ Using selenium, make sure to have chromedriver updated.
 """
 from selenium import webdriver
 import os
+import time
 
 website = "https://osu.ppy.sh/beatmapsets?s=ranked"
 driver = webdriver.Chrome(os.getcwd() + "/chromedriver")
 tsvFile = "data/osuRanked.tsv"
 
 def MainPage():
+    time.sleep(1)
     driver.get(website)    
-    links = [i.get_attribute("href") for i in driver.find_elements_by_xpath("//a[@title]") if "songs" in i.get_attribute("href")]
+    #links = [i.get_attribute("href") for i in driver.find_elements_by_xpath("//a[@title]") if "songs" in i.get_attribute("href")]
+    
+    links = [i.get_attribute("href") for i in driver.find_elements_by_class_name("beatmapset-panel__header") if 'beatmapsets' in i.get_attribute("href")]
     links = list(dict.fromkeys(links))
-    text = ""
-    for i in links:
-        text = text + i + "\n"
-    with open("links.txt", "w+") as file:
-        file.write(text)
     return links
         
 def Categories():
-    div = driver.find_element_by_class_name("bsaber-categories")
-    links = [i.text for i in div.find_elements_by_tag_name("a")]
-    if len(links) == 0:
-        return "-"
-    else:  
-        text = ""
-        for i in links:
-            text = text + str(i) + ", "
-        return text[:-2]
-
+    time.sleep(1)
+    elem = driver.find_elements_by_class_name("beatmapset-info__header")
+    
+    for i in elem:
+        if 'Tags' in i.text:
+            parent = i.find_element_by_xpath("..")
+            tags = parent.find_elements_by_tag_name("div")[0]
+            links = [j.text for j in tags.find_elements_by_tag_name("a")]
+            if len(links) == 0:
+                return "-"
+            else:  
+                text = ""
+                for i in links:
+                    text = text + str(i) + ", "
+                return text[:-2]
+    
 def GetYoutube(name):
     driver.get("https://www.youtube.com/results?search_query=" + name)    
     firstResult = driver.find_element_by_id("video-title").get_attribute("href")
     return firstResult
-    
-def GetDate():
-    date = driver.find_element_by_tag_name("time").text
-    if "day" in date:
-        num = int(date.split(" ")[0]) * 1
-    elif "week" in date:
-        num = int(date.split(" ")[0]) * 7
-    elif "month" in date:
-        num = int(date.split(" ")[0]) * 30
-    elif "hour" in date:
-        num = int(date.split(" ")[0]) / 24
-    return num
 
 def Page(page):
-    print(page)
     driver.get(page)
-    song = driver.find_element_by_class_name("entry-title").text
-    mapper = Mapper()
+    time.sleep(1)
+    song = driver.find_elements_by_class_name("beatmapset-header__details-text")[0].text
+    artist = driver.find_elements_by_class_name("beatmapset-header__details-text")[1].text
+    mapper = driver.find_element_by_class_name("beatmapset-mapping__user").get_attribute("href")
+    
     categories = Categories()
-    scores = GetScore() 
-    days = GetDate()
     stream = GetYoutube(song)
+
+    Row(song + " " + artist, mapper, categories, stream, page)
     
-    Row(song, mapper, categories, scores, stream, days, page)
     
-    
-def Row(song, mapper, categories, scores, stream, days, page):
-    #ratio = str(int(scores[0]-scores[1]/days))
-    #ratio formula for excel/sheet
-    ratio = "=(" + str(scores[0]) + "-" + str(scores[1]) + ")/" + str(days) 
+def Row(song, mapper, categories, stream, page):
     text = str(song) + "\t - \t" + str(stream) + "\t" + str(categories) + "\t" 
-    text = text + ratio + "\t" + str(page) + "\t" + str(mapper) + "\n"
+    text = text + "\t" + str(page) + "\t" + str(mapper) + "\n"
     
     with open(tsvFile, "r") as file:
         base = file.read()
@@ -79,7 +69,7 @@ def Row(song, mapper, categories, scores, stream, days, page):
 
 
 # Create/rewrites the file
-header = "Song \t Genre \t Stream \t Tags \t Likes/Days \t Map \t Mapper \n"
+header = "Song \t Genre \t Stream \t Tags \t Score \t Map \t Mapper \n"
 with open(tsvFile, "w+") as file:
     file.write(header)
 
